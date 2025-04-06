1771688a751afb50d24254f3d42e47f43cba5d4a
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-04-06 02:28:52 -0400

    se arreglo la rotacion del tablero

--- src/javascript/tablero.js
+++ src/javascript/tablero.js
@@ -4,12 +4,16 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.mo
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('/assets/img/monopolio.jpg');
 
-const tableroGeometry = new THREE.BoxGeometry(5, 5,0.3);
+const tableroGeometry = new THREE.BoxGeometry(0.3,5,5);
 
 //const tableroMaterial = new THREE.MeshBasicMaterial({ color: 0x008000, side: THREE.DoubleSide });
-const tableroMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Apply texture
+const tableroColor = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Aplica color al mesh
+const tableroMaterial = new THREE.MeshBasicMaterial({ 
+    map:texture,
+    side: THREE.DoubleSide }); // Aplica la textura importada
 const tablero = new THREE.Mesh(tableroGeometry, tableroMaterial);
-tablero.rotation.x = -Math.PI / 2;
+tablero.rotation.z = Math.PI / 2;
+
 tablero.position.set(0,0,0);
  
 export {tablero};
