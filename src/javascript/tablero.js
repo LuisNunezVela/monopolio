b16a2d1c08085132730d57cb6c88f6a51e45e245
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-04-17 19:10:57 -0400

    cambios

--- src/javascript/tablero.js
+++ src/javascript/tablero.js
@@ -1,55 +1,44 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
 
-// Load texture
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/assets/img/monopolio.jpg');
+
 const rotacionCorregida = -Math.PI / 2
+const tablero_txt = textureLoader.load('/assets/img/monopolio.jpg');
 
+const tableroMaterial = new THREE.MeshBasicMaterial({ 
+    map:tablero_txt,
+    side: THREE.DoubleSide });
 
-texture.flipY = false;
-texture.rotation = rotacionCorregida;
-texture.center.set(0.5, 0.5);
+const verde = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const blanco = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
+const casillas = [];
+const casillasGroup = new THREE.Group();
 const cornerSize = 10;
 const sideWidth = 5;
 const sideHeight = 10;
 const espacio = 1.1;
 const numCasillasxlado = 11;
-
 const tableroSize = 9 * sideWidth + 2 * cornerSize;
 
+// geometrias
 const tableroGeometry = new THREE.PlaneGeometry(tableroSize,tableroSize);
+const casillaGeometry = new THREE.PlaneGeometry(sideWidth,sideHeight);
 
-//const tableroMaterial = new THREE.MeshBasicMaterial({ color: 0x008000, side: THREE.DoubleSide });
-const tableroColor = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
-
-const tableroMaterial = new THREE.MeshBasicMaterial({ 
-    map:texture,
-    side: THREE.DoubleSide });
 const tablero = new THREE.Mesh(tableroGeometry, tableroMaterial);
-tablero.rotation.x = -Math.PI / 2;
-
+tablero.rotation.x = rotacionCorregida;
 tablero.position.set(0,0,0);
 
-const geometry_casilla = new THREE.PlaneGeometry(sideWidth,sideHeight);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-const casillasGroup = new THREE.Group();
-
-
-
-const casillas = [];
-
 for (let i = 0; i < numCasillasxlado * 4; i++) {
-    const casilla = new THREE.Mesh(geometry_casilla, material);
+    const casilla = new THREE.Mesh(casillaGeometry, verde);
     casilla.position.set(-tableroSize/2 + i * sideWidth * espacio, 0.01, 0 );
-    casilla.rotation.x = (rotacionCorregida);
+    casilla.rotation.x = rotacionCorregida;
+
+    // colocar if numCasillasxlado < 11, etc
 
     casillas.push(casilla);
     casillasGroup.add(casilla);
   }
-
 
 export {tablero};
 export {tableroSize};
