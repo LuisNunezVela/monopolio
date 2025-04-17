e16e260fd0714e45e295bf71d7daa87c3bf8804a
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-04-17 05:29:44 -0400

    cambios

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -5,9 +5,8 @@ import {casillasGroup} from './tablero.js'
 // Crear la escena y la cámara
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = 60;
-camera.position.z = 0;
-camera.position.y = 60;
+camera.position.set(0,80,0);
+
 const lookAtTarget = new THREE.Vector3(0, 0, 0);
 camera.lookAt(lookAtTarget);
 
--- src/javascript/tablero.js
+++ src/javascript/tablero.js
@@ -13,8 +13,10 @@ texture.center.set(0.5, 0.5);
 const cornerSize = 10;
 const sideWidth = 5;
 const sideHeight = 10;
+const espacio = 1.1;
+const numCasillasxlado = 11;
 
-const tableroSize = 11 * sideWidth + 2 * cornerSize;
+const tableroSize = 9 * sideWidth + 2 * cornerSize;
 
 const tableroGeometry = new THREE.PlaneGeometry(tableroSize,tableroSize);
 
@@ -26,13 +28,9 @@ const tableroMaterial = new THREE.MeshBasicMaterial({
     map:texture,
     side: THREE.DoubleSide });
 const tablero = new THREE.Mesh(tableroGeometry, tableroMaterial);
-tablero.rotation.x = Math.PI / 2;
+tablero.rotation.x = -Math.PI / 2;
 
 tablero.position.set(0,0,0);
- 
-
-
-
 
 const geometry_casilla = new THREE.PlaneGeometry(sideWidth,sideHeight);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -43,13 +41,9 @@ const casillasGroup = new THREE.Group();
 
 const casillas = [];
 
-let x = 0;
-let y = 0
-
-
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < numCasillasxlado * 4; i++) {
     const casilla = new THREE.Mesh(geometry_casilla, material);
-    casilla.position.set(-20+ i * 5.5, 0.01, 28 );
+    casilla.position.set(-tableroSize/2 + i * sideWidth * espacio, 0.01, 0 );
     casilla.rotation.x = (rotacionCorregida);
 
     casillas.push(casilla);
