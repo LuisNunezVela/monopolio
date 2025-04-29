b5b7dc3a0972efa18c40ce5692809f303f163238
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-04-29 03:19:54 -0400

    arreglo rotacion casillas

--- src/javascript/tablero.js
+++ src/javascript/tablero.js
@@ -4,9 +4,14 @@ const textureLoader = new THREE.TextureLoader();
 
 const rotacionCorregida = -Math.PI / 2
 const tablero_txt = textureLoader.load('/assets/img/monopolio.jpg');
+const casilla_txt = textureLoader.load('/assets/img/casilla.png')
 
 const tableroMaterial = new THREE.MeshBasicMaterial({ 
     map:tablero_txt,
+    side: THREE.DoubleSide });
+
+const casillaMaterial = new THREE.MeshBasicMaterial({ 
+    map:casilla_txt,
     side: THREE.DoubleSide });
 
 const verde = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -17,22 +22,21 @@ const casillasGroup = new THREE.Group();
 const cornerSize = 10;
 const sideWidth = 5;
 const sideHeight = 10;
-const espacio = 1.05;
+const espacio = 1.0;
 const espacioEsq = 1.70;
 const numCasillasxlado = 11;
 const tableroSize = 65;
-//const tableroSize = 9 * sideWidth + 2 * cornerSize;
 
 // geometrias
 const tableroGeometry = new THREE.PlaneGeometry(tableroSize,tableroSize);
 const casillaGeometry = new THREE.PlaneGeometry(sideWidth,sideHeight);
 
-const tablero = new THREE.Mesh(tableroGeometry, tableroMaterial);
+const tablero = new THREE.Mesh(tableroGeometry, verde);
 tablero.rotation.x = rotacionCorregida;
 tablero.position.set(0,0,0);
 
 for (let i = 0; i < numCasillasxlado * 4 - 4; i++) {
-  const casilla = new THREE.Mesh(casillaGeometry, verde);
+  const casilla = new THREE.Mesh(casillaGeometry, casillaMaterial);
   casilla.rotation.x = rotacionCorregida;
 
   if (i < 10) {
@@ -41,10 +45,10 @@ for (let i = 0; i < numCasillasxlado * 4 - 4; i++) {
     // Restaurar la geometría rectangular para las demás casillas
     casilla.geometry = new THREE.PlaneGeometry(sideWidth, sideHeight);
     casilla.position.set(
-        (tableroSize / 2) - (cornerSize / 2) - i * sideWidth * espacio, 0.01, (tableroSize / 2) - (cornerSize / 2)
-    ); 
-      
-  } else if (i < 20) {
+        (tableroSize / 2) - (cornerSize / 2) - i * sideWidth * espacio, 0.01, (tableroSize / 2) - (cornerSize / 2)); 
+    } 
+ 
+    else if (i < 20) {
       // Segunda parte: bajar (Z-)
       const j = i - 10;
       casilla.position.set(
@@ -52,8 +56,9 @@ for (let i = 0; i < numCasillasxlado * 4 - 4; i++) {
           0.01,
           (tableroSize / 2) - (cornerSize / 2) - (j + 1) * sideWidth * espacio
       );
-      casilla.rotation.z = Math.PI / 2; // Rotar 90°
-  } else if (i < 30) {
+      casilla.rotation.z = -Math.PI / 2; // Rotar 90°
+  } 
+  else if (i < 30) {
       // Tercera parte: ir a la derecha (X+)
       const j = i - 20;
       casilla.position.set(
@@ -61,6 +66,7 @@ for (let i = 0; i < numCasillasxlado * 4 - 4; i++) {
           0.01,
           -(tableroSize / 2) + (cornerSize / 2)
       );
+      casilla.rotation.x = Math.PI / 2;
   } else {
       // Cuarta parte: subir (Z+)
       const j = i - 30;
